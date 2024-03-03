a661b243e54312a82039feeeccd6281e09dee646
Cosmic_Crafter <138387813+Cosmic-Crafter-1@users.noreply.github.com>
2024-03-03 07:22:52 +0530

    Added the point class

---
+++ js/primitives/point.js
@@ -0,0 +1,19 @@
+
+class Point {
+	constructor(x,y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	draw(ctx, size = 18, color = "black") {
+		// To draw it as a circle
+		const radius = size/2;
+		
+		//  This starts a new path for drawing on the canvas. It's like lifting a pen off the paper to start drawing in a new place.
+		ctx.beginPath();
+		ctx.fillStyle = color;
+		// We want a full circle so 0 deg to 2 PI = 360 deg.
+		ctx.arc(this.x, this.y, rad, 0, Math.PI * 2);
+		ctx.fill();
+	}
+}
