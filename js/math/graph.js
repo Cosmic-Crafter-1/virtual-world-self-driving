09d1ac2fd570e05750eac24e571c1c64981f5321
Cosmic_Crafter <138387813+Cosmic-Crafter-1@users.noreply.github.com>
2024-03-03 09:25:02 +0530

    Implemented addPoint function.

--- js/math/graph.js
+++ js/math/graph.js
@@ -5,6 +5,24 @@ class Graph {
 		this.segments = segments;
 	}
 
+	addPoint(point) {
+		this.points.push(point);
+	}
+
+	containsPoint(point) {
+		// Find method loops over all points and returns True/False.
+		// We need to implement the equals method.
+		return this.points.find((p) => p.equals(point));
+	}
+
+	tryAddPoint(point) {
+		if (!this.containsPoint(point)) {
+			this.addPoint(point);
+			return true;
+		}
+		return false;
+	}
+
 	draw(ctx) {
 
 		for (const seg of this.segments) {
--- js/primitives/Point.js
+++ js/primitives/Point.js
@@ -5,9 +5,13 @@ class Point {
 		this.y = y;
 	}
 
+	equals(point) {
+		return this.x == point.x && this.y == point.y;
+	}
+
 	draw(ctx, size = 18, color = "black") {
 		// To draw it as a circle
-		const radius = size/2;
+		const radius = size / 2;
 		
 		//  This starts a new path for drawing on the canvas. It's like lifting a pen off the paper to start drawing in a new place.
 		ctx.beginPath();
